a374e9ed813b397d360b90847ba8bf79b4b3b68c
Austin Zhu <45208549+anotherbeaver@users.noreply.github.com>
2025-09-10 09:12:28 -0700

    Changed to molecule instead of data input file

--- check_potential.py
+++ check_potential.py
@@ -1,4 +1,18 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interp1d
+
+
+def shift_and_sum(r1, e1, r2, e2, x0):
+    # interpolate U2
+    f2 = interp1d(r2, e2, kind='linear', bounds_error=False, fill_value=0.0)
+    
+    # evaluate shifted U2 at r1 positions
+    e2_shifted = f2(r1 - x0)  # shift by x0
+    
+    # sum potentials
+    e_total = e1 + e2_shifted
+    return e2_shifted, e_total
 
 def plot_summed_potentials():
     pot_1 = "data/pair_coeff_1_2.txt"
@@ -25,17 +39,22 @@ def plot_summed_potentials():
     r1, e1 = read_pot(pot_1)
     r2, e2 = read_pot(pot_2)
 
+    r1, e1 = np.array(r1), np.array(e1)
+    r2, e2 = np.array(r2), np.array(e2)
+
     if len(r1) != len(r2):
         raise ValueError("Potential tables have different r-grid lengths!")
+    
+    x0 = 0.9
+    e2_shifted, e_total = shift_and_sum(r1, e1, r2, e2, x0)
+    # e_total = e1 + e2
 
-    e_total = [e1[i] + e2[i] for i in range(len(r1))]
-
-    # plt.plot(r2, e1, label='Backbone-Patch')
-    plt.plot(r2, e2, label='Patch-Patch')
-    # plt.plot(r2, e_total, label='Sum', linestyle='--')
+    plt.plot(r1, e1, label='Backbone-Patch (WCA)')
+    plt.plot(r1, e2, label='Patch-Patch (Gaussian)')
+    plt.plot(r1, e_total, label='Sum', linestyle='--')
     plt.xlabel('r')
     plt.ylabel('U(r)')
-    # plt.ylim(-5, 5)
+    plt.ylim(-40, 40)
     plt.legend()
     plt.title('Summed Interaction Potentials')
     # plt.savefig('data/summed_potentials.png', dpi=300)
--- generate_molecule.py
+++ generate_molecule.py
@@ -1,20 +1,9 @@
 """
-Generate data file for LAMMPS simulation
+Similar to generate_data.py but generates a .molecule file, which should be easier to use to generate random configs
 
-The output data file will be a replicable module that can be used to create a larger system.
-
-The general structure of the molecule is as follows:
-    Backbone: 1 - 1 - 1 - 1 - 1 - 1 - 1 - 1
-                      |
-                  Dummy: 1
-                      |
-                  Patch: 2
-
-    Where '1' is a backbone bead, '2' is an amine patch bead, and the dummy bead (also type '1') is used to create a rigid bond angle between the backbone and the patch.
-
-Output:
-    data.particles: LAMMPS data file containing single molecule information
+Note that the formatting of individual atoms has been split across the sections for molecule entries
 """
+
 
 chain_length = 25  # number of monomers in a chain
 amine_spacing = 10  # spacing between amine groups in a chain
@@ -23,41 +12,49 @@ BOX_Y = 2.45 # height of one monomer-monomer distance (1) + patch height (.45) +
 BOX_Z = 1.2 # padding (1.2)
 
 
-def generate_data_file(filename='data/data.particles'):
+def generate_molecule_file(filename='data/polymer.molecule'):
     if amine_spacing > chain_length:
         raise ValueError("Amine spacing cannot be greater than chain length.")
     amine_positions = list(range(0, chain_length, amine_spacing))
     if amine_positions[-1] == chain_length - 1:
         print("Warning: Last monomer is an amine patch. This affects chain length.")
+
+    N = chain_length + len(amine_positions) * 2  # total atoms
     
     with open(filename, 'w') as f:
         f.write("LAMMPS Description\n\n")
 
         # Counts 
-        f.write(f"{chain_length + len(amine_positions) * 2} atoms\n") # each amine position has 2 atoms (patch + dummy)
-        f.write(f"{chain_length - 1 + len(amine_positions) * 2} bonds\n") # each amine position has 2 bonds (to dummy and dummy to patch)
+        f.write(f"{N} atoms\n") # each amine position has 2 atoms (patch + dummy)
+        f.write(f"{N - 1} bonds\n") # each amine position has 2 bonds (to dummy and dummy to patch)
 
-        # Types
-        f.write("2 atom types\n") # backbone and amine patch
-        f.write("2 bond types\n") # backbone bond and patch-dummy bond
+        # # Masses
+        # f.write("\nMasses\n\n")
+        # f.write("1 1.0\n")    # backbone + dummy mass
+        # f.write("2 1\n\n")   # patch mass TODO: figure out if we can reduce this without blowing up the simulation
 
-        # box is sized for a single chain with padding
-        f.write(f"0.0 {BOX_X} xlo xhi\n")
-        f.write(f"0.0 {BOX_Y} ylo yhi\n")
-        f.write(f"0.0 {BOX_Z} zlo zhi\n\n")
-
-        f.write("Masses\n\n")
-        f.write("1 1.0\n")    # backbone + dummy mass
-        f.write("2 1\n\n")   # patch mass TODO: figure out if we can reduce this without blowing up the simulation
-
-        f.write("Atoms\n\n")
+        f.write("Coords\n\n")
         for i in range(chain_length):
-            f.write(f"{i+1} 1 1 {i+1} 0.0 0.0\n")
+            f.write(f"{i+1} {i+1}.0 0.0 0.0\n")
         for idx, pos in enumerate(amine_positions):
             dummy_id = chain_length + idx * 2 + 1
             patch_id = chain_length + idx * 2 + 2
-            f.write(f"{dummy_id} 1 1 {pos + 1} 1.0 0.0\n")
-            f.write(f"{patch_id} 2 2 {pos + 1} 1.45 0.0\n")
+            f.write(f"{dummy_id} {pos + 1} 1.0 0.0\n")
+            f.write(f"{patch_id} {pos + 1} 1.45 0.0\n")
+
+        f.write("\nTypes\n\n")
+        for i in range(chain_length):
+            f.write(f"{i+1} 1\n")
+        for idx, pos in enumerate(amine_positions):
+            dummy_id = chain_length + idx * 2 + 1
+            patch_id = chain_length + idx * 2 + 2
+            f.write(f"{dummy_id} 1\n")
+            f.write(f"{patch_id} 2\n")
+
+
+        f.write("\nMasses\n\n")
+        for i in range(N):
+            f.write(f"{i+1} 1.0\n")
 
         f.write("\nBonds\n\n")
         for i in range(chain_length - 1):
@@ -87,4 +84,4 @@ def generate_chain_positions():
 
 if __name__ == "__main__":
     generate_chain_positions()
-    generate_data_file()
+    generate_molecule_file()
