48dc7bd8c42dcf95fd59403ea45f19d2e4299d8f
Austin Zhu <45208549+anotherbeaver@users.noreply.github.com>
2025-10-10 11:38:54 -0700

    Autocorrelation problem going above 1

---
+++ generate_molecule_no_dummy.py
@@ -0,0 +1,99 @@
+"""
+Similar to generate_data.py but generates a .molecule file, which should be easier to use to generate random configs
+
+Note that the formatting of individual atoms has been split across the sections for molecule entries
+"""
+
+
+
+
+import argparse
+
+
+def generate_molecule_file(filename='data/polymer.molecule',
+                           chain_length=25,
+                           amine_spacing=5,
+                           backbone_to_patch=0.4):
+    
+    BOX_X = chain_length + 2 # length of monomer-monomer distances with extra padding (1)
+    BOX_Y = 2.45 # height of one monomer-monomer distance (1) + patch height (.45) + padding (1)
+    BOX_Z = 1.2 # padding (1.2)
+
+
+    if amine_spacing > chain_length:
+        raise ValueError("Amine spacing cannot be greater than chain length.")
+    amine_positions = list(range(amine_spacing, chain_length, amine_spacing))
+    if amine_positions[-1] == chain_length - 1:
+        print("Warning: Last monomer is an amine patch. This affects chain length.")
+
+    N = chain_length + len(amine_positions)  # total atoms
+    
+    with open(filename, 'w') as f:
+        f.write("LAMMPS Description\n\n")
+
+        # Counts 
+        f.write(f"{N} atoms\n") # each amine position has 2 atoms (patch + dummy)
+        f.write(f"{N - 1} bonds\n") # each amine position has 2 bonds (to dummy and dummy to patch)
+
+        # # Masses
+        # f.write("\nMasses\n\n")
+        # f.write("1 1.0\n")    # backbone + dummy mass
+        # f.write("2 1\n\n")   # patch mass TODO: figure out if we can reduce this without blowing up the simulation
+
+        f.write("Coords\n\n")
+        for i in range(chain_length):
+            f.write(f"{i+1} {i+1}.0 0.0 0.0\n")
+        for idx, pos in enumerate(amine_positions):
+            patch_id = chain_length + idx + 1
+            f.write(f"{patch_id} {pos + 1} {backbone_to_patch} 0.0\n")
+        f.write("\nTypes\n\n")
+        for i in range(chain_length):
+            f.write(f"{i+1} 1\n")
+        for idx, pos in enumerate(amine_positions):
+            patch_id = chain_length + idx + 1
+            f.write(f"{patch_id} 2\n")
+
+
+        f.write("\nMasses\n\n")
+        for i in range(N):
+            f.write(f"{i+1} 1.0\n")
+
+        f.write("\nBonds\n\n")
+        for i in range(chain_length - 1):
+            f.write(f"{i+1} 1 {i+1} {i+2}\n")
+        for idx, pos in enumerate(amine_positions):
+            patch_id = chain_length + idx + 1
+            f.write(f"{chain_length - 1 + idx * 2} 2 {pos + 1} {patch_id}\n")
+
+def generate_chain_positions():
+    """
+    Generate positions for chains of particles.
+    
+    Returns:
+        List of tuples: (particle_id, x, y, z)
+    """
+    # positions = []
+    # for chain_id in range(num_chains):
+    #     for i in range(chain_length):
+    #         x = chain_id * amine_spacing
+    #         y = 0.0
+    #         z = i * amine_spacing
+    #         positions.append((chain_id * chain_length + i + 1, x, y, z))
+    # return positions
+    pass
+
+if __name__ == "__main__":
+    # generate_chain_positions()
+    args = argparse.ArgumentParser()
+    args.add_argument('--filename', type=str, default='data/polymer.molecule', help='Output filename')
+    args.add_argument('--chain_length', type=int, default=25, help='Length of the polymer chain')
+    args.add_argument('--amine_spacing', type=int, default=5, help='Spacing between amine patches')
+    args.add_argument('--backbone_to_patch', type=float, default=0.4, help='Distance from backbone to patch')
+    args = args.parse_args()
+    print(args)
+    generate_molecule_file(
+        filename=args.filename,
+        chain_length=args.chain_length,
+        amine_spacing=args.amine_spacing,
+        backbone_to_patch=args.backbone_to_patch
+    )
--- run.sh
+++ run.sh
@@ -2,16 +2,43 @@
 
 LAMMPS=../lammps/build/lmp
 INPUT=in.amine_polymers_sh
-LOGDIR=data
+LOGDIR=logs
 
 mkdir -p "$LOGDIR"
 
 # scales=(50.0 60.0 70.0 80.0 90.0 150.0)
 # scales=(50.0 60.0 70.0)
-scales=(49.0 99.0 149.0 200.0 300.0 400.0)
+# scales=(49.0 99.0 149.0 200.0 300.0 400.0)
 # lengths=(0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7)
 # lengths=(0.6 0.65 0.7)
 
+# scales=(0 40 50 60)
+# scales=(0)
+scales=(50)
+
+# lengths=(0.2 0.3 0.4 0.5 0.6 0.7)
+# lengths=(0.2 0.3)
+lengths=(0.5)
+
+n_patch=(1 2 3 4)
+
+for n in "${n_patch[@]}"; do
+  echo "Generating molecule with $n amine patches."
+  python3 generate_molecule_no_dummy.py --amine_spacing $n
+  for scale in "${scales[@]}"; do
+    for length in "${lengths[@]}"; do
+      echo "Launching hbond_energy_scale=$scale and hbond_distance_cutoff=$length"
+      mpirun -np 12 --oversubscribe "$LAMMPS" -in "$INPUT" \
+        -var patch_gauss_A_strength "$scale" \
+        -var patch_bond_r0 "$length" \
+        -var output_file "$LOGDIR/patch_n_${n}_${scale}_${length}.dat"
+    done
+  done
+done
+
+
+wait
+echo "All LAMMPS jobs finished."
 
 
 
@@ -33,16 +60,16 @@ scales=(49.0 99.0 149.0 200.0 300.0 400.0)
 #   python3 find_reversible_bonds.py --traj "data/energy_${scale}.lammpstrj" --output "data/amine_bonds_${scale}.csv"
 # done
 
-wait
-echo "All bond finding jobs finished."
+# wait
+# echo "All bond finding jobs finished."
 
-for scale in "${scales[@]}"; do
-  # find multiple bonds
-  output_csv="data/amine_multiplets_${scale}.csv"
-  python3 find_multiplets.py --input "data/amine_bonds_${scale}.csv" --output "$output_csv"
-  echo "Job with hbond_energy_scale=$scale completed."
-done
+# for scale in "${scales[@]}"; do
+#   # find multiple bonds
+#   output_csv="data/amine_multiplets_${scale}.csv"
+#   python3 find_multiplets.py --input "data/amine_bonds_${scale}.csv" --output "$output_csv"
+#   echo "Job with hbond_energy_scale=$scale completed."
+# done
 
-wait
-echo "All jobs finished."
+# wait
+# echo "All jobs finished."
 
